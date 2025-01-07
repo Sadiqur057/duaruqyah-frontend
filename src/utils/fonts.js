6c323549ff78c53474e46b13d8c77c555fd7c979
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-01-07 19:12:44 +0600

    added fonts

---
+++ src/utils/fonts.js
@@ -0,0 +1,11 @@
+import { Poppins, Inter } from 'next/font/google';
+
+export const inter = Inter({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+});
+
+export const poppins = Poppins({
+  weight: ['200', '300', '400', '500', '600', '700'],
+  subsets: ['latin'],
+});
