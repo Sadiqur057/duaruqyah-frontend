4476e44fed58dd99a5ae471542f8fdbd5a41034a
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-01-07 12:13:06 +0600

    added redirection content

---
+++ src/components/shared/RedirectionContent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const RedirectionContent = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white relative px-4">
+      <div className="text-center z-10">
+        <h2 className="my-3 font-semibold text-[36px] text-primary mb-0">
+          SUBHAN ALLAH
+        </h2>
+        <h3 className="text-2xl md:text-3xl font-semibold">
+          There is nothing in this page
+        </h3>
+        <p className="max-w-md text-light mx-auto">
+          We&apos;re sorry, the page you requested could not be found.
+          Redirecting you to Dua's Page
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RedirectionContent;
