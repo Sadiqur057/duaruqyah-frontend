8f0f43a4b5a398077258086e91462134ffcae8b7
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-01-07 23:17:46 +0600

    added metadata title

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,11 @@
 import "./globals.css";
 import { inter } from "@/utils/fonts";
 
-export default async function RootLayout({ children }) {
+export const metadata= {
+  title: "Dua & Ruqyah | All Duas"
+}
 
+export default async function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-body`}>
